c5579aa0449bf901909d174bfe90bc6955bba937
Matt Glover <matt@duchysoftware.com>
2013-01-01 00:28:17 +0000

    Initial Commit

---
+++ MoreColorsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoreColorsDemo
+//
+//  Created by Matt Glover on 01/01/2013.
+//  Copyright (c) 2013 Duchy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
